3a19a19b846ef895a2b0d660647d340633eb770d
Salem Hilal <salemhilal@gmail.com>
2013-11-13 20:54:24 -0500

    Autocomplete works, yo.

--- js/app.js
+++ js/app.js
@@ -3,3 +3,5 @@ console.log("app initialized");
 SC.initialize({
     client_id: '124d98e7c716fd363f37574473ddf687'
 });
+
+//http://api.soundcloud.com/users/3207.json?client_id=124d98e7c716fd363f37574473ddf687
--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,24 +1,72 @@
-function AutocompleteCtrl($scope) {
+function CreateChannel($scope) {
+    // How long to wait to send out autocomplete queries
     var debounceWait = 300;
+    // Number of query results to display
+    var suggestionCount = 8;
 
     // Autocomplete results
     $scope.results = [];
 
-    // Autocomplete query
-    $scope.query = _.debounce(function() {
-        // Query soundcloud based on what's in the text box.
-        SC.get('/users', { q: $scope.queryText }, function(users) {
+    // List of artists
+    $scope.artists = [];
 
-            // Clear results
-            $scope.results = [];
+    $scope.queries = false;
 
-            // Populate with new ones
+    var autocomplete = _.debounce(function() {
+        var query = $scope.queryText ? $scope.queryText.trim() : "";
+        console.log("Query", query);
+
+        // No sense querying if there's nothing to query.
+        if(query.length === 0) {
             $scope.$apply(function() {
-               $scope.results = users;
+                $scope.queries = false;
+                $scope.results = [];
             });
+            return;
+        }
 
+        // Query soundcloud based on what's in the text box.
+        SC.get('/users', { q: query }, function(users) {
             console.log(users);
+            $scope.$apply(function() {
+                $scope.queries = false;
 
+                // Clear results
+                $scope.results = [];
+
+                if(users.length === 0)
+                    return;
+
+                // Populate with new ones
+                $scope.results = users.slice(0, suggestionCount);
+                $scope.results[0].selected = true;
+            });
         });
     }, debounceWait);
+
+    // Autocomplete query
+    $scope.query = function(event) {
+
+        var code = event.keyCode;
+
+        // Clear input if we pressed delete or an alphanumeric key
+        // input = 8 (delete) or btn 48 - 90, inclusive (numbers and letters)
+        console.log(code);
+        if(code == 8 || (code >= 48 && code <= 90)) {
+            console.log("Clearing results");
+            $scope.results = [];
+        }
+
+        $scope.queries = true;
+        autocomplete();
+    };
+
+    // What step we're on in input.
+    // 0 = Playlist Name
+    // 1 = Adding artists
+    // 2 = Re-editing name.
+    $scope.step = 0;
+
+    $scope.editName = true;
+
 }
