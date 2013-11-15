3711e44b9c449191b4d28348f71ee9ec00a278cd
Salem Hilal <salemhilal@gmail.com>
2013-11-14 21:00:02 -0600

    Localized libraries.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log("app initialized");
 
-SC.initialize({
+/*SC.initialize({
     client_id: '124d98e7c716fd363f37574473ddf687'
-});
+});*/
 
 //http://api.soundcloud.com/users/3207.json?client_id=124d98e7c716fd363f37574473ddf687
